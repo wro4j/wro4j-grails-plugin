e956a1c964eefaecbd44aad6380146e1999d5fe8
Craig Andrews <candrews@integralblue.com>
2014-01-24 11:03:00 -0500

    Start work on 1.7.4 by setting the version to 1.7.4-SNAPSHOT

--- Wro4jGrailsPlugin.groovy
+++ Wro4jGrailsPlugin.groovy
@@ -10,7 +10,7 @@ import wro4j.grails.plugin.WroConfigHandler
 import wro4j.grails.plugin.WroDSLHandler
 
 class Wro4jGrailsPlugin {
-  def version = "1.7.3"
+  def version = "1.7.4-SNAPSHOT"
   def grailsVersion = "1.3.7 > *"
   def pluginExcludes = [
       "grails-app/views/index.gsp",
